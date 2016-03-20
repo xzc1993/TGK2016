fdc872f207d224d0ffedf27ba0883133f85352cc
Mateusz Ziebura <mateusz.ziebura93@gmail.com>
2016-03-20 15:19:26 +0100

    Created draft terrain with some random trees

---
+++ EndlessForest/Assets/ProceduralTerrain/TerrainChunk/TerrainChunk.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using LibNoise.Unity.Generator;
+using Wasabimole.ProceduralTree;
+
+public class TerrainChunk
+{
+    public Terrain Terrain { get; private set; }
+    public TerrainCollider TerrainCollider { get; private set; }
+    public Texture2D Texture { get; private set; }    
+
+    public int ChunkX { get; set; }
+    public int ChunkZ { get; set; }
+    public int HeightmapResolution { get; set; }
+    public float ChunkLength { get; set; }
+    public float ChunkHeight { get; set; }
+    public Material Material { get; set; }
+    public ProceduralTree TreePrefab { get; set; }
+
+    public void Render()
+    {
+        var terrainData = CreateTerrainData();
+        var terrainGameObject = CreateTerrain(terrainData);
+        Terrain = terrainGameObject.GetComponent<Terrain>();
+        TerrainCollider = terrainGameObject.GetComponent<TerrainCollider>();
+        ApplyTextures(Terrain);
+        RenderTrees(Terrain);
+        Terrain.Flush();
+    }
+
+    private TerrainData CreateTerrainData()
+    {
+        TerrainData terrainData = new TerrainData();
+        terrainData.heightmapResolution = HeightmapResolution;
+        terrainData.alphamapResolution = HeightmapResolution;
+        terrainData.size = new Vector3(ChunkLength, ChunkHeight, ChunkLength);
+        terrainData.SetHeights(0, 0, CreateHeightmap());
+        return terrainData;
+    }
+
+    private GameObject CreateTerrain(TerrainData terrainData)
+    {
+        var terrainGameObject = Terrain.CreateTerrainGameObject(terrainData);
+        terrainGameObject.transform.position = new Vector3(ChunkX * ChunkLength, 0, ChunkZ * ChunkLength);
+        return terrainGameObject;
+    }
+
+    private float[,] CreateHeightmap()
+    {
+        var heightmap = new float[HeightmapResolution, HeightmapResolution];
+        for (var zRes = 0; zRes < HeightmapResolution; zRes++)
+        {
+            for (var xRes = 0; xRes < HeightmapResolution; xRes++)
+            {
+                var xCoordinate = ChunkX + ((float)xRes /( HeightmapResolution - 1f));
+                var zCoordinate = ChunkZ + ((float)zRes /( HeightmapResolution - 1f));
+                heightmap[zRes, xRes] = Mathf.PerlinNoise(xCoordinate, zCoordinate);
+            }
+        }
+        return heightmap;
+    }
+
+    private void ApplyTextures(Terrain terrain)
+    {
+        Texture = new Texture2D((int)ChunkLength, (int)ChunkLength);
+        Material.SetTexture(1, Texture);
+        Terrain.materialTemplate = Material;
+        Terrain.materialType = Terrain.MaterialType.Custom;
+    }
+
+    private void RenderTrees(Terrain terrain)
+    {
+        var xRes = ((ChunkX) * HeightmapResolution + 1);
+        var zRes = ((ChunkZ) * HeightmapResolution + 1);
+        var pos = Terrain.GetPosition();
+        var position = new Vector3(pos.x, terrain.terrainData.GetHeight((int)xRes, (int)zRes), pos.z);
+        TreePrefab.Seed = Random.seed;
+        var tree = GameObject.Instantiate(TreePrefab, position, Quaternion.identity);
+        tree.name = Random.value.ToString();
+    }
+}
---
+++ EndlessForest/Assets/ProceduralTerrain/TerrainManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Wasabimole.ProceduralTree;
+
+public class TerrainManager : MonoBehaviour
+{
+    public Material Material;
+    [Range(5, 10)]
+    public ProceduralTree ProceduralTreePrefab;
+    public int HeightmapResolution;
+    public float ChunkLength;
+    public float ChunkHeight;
+   
+
+    // Use this for initialization
+    void Start ()
+	{
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j <3; j++)
+            {
+                TerrainChunk terrainChunk = new TerrainChunk()
+                {
+                    ChunkX = i,
+                    ChunkZ = j,
+                    HeightmapResolution = (int) Mathf.Pow(2, HeightmapResolution) + 1,
+                    ChunkHeight = ChunkHeight,
+                    ChunkLength = ChunkLength,
+                    Material = Material,
+                    TreePrefab = ProceduralTreePrefab
+                };
+                terrainChunk.Render();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+}
